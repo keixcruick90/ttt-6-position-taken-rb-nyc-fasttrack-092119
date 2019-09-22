3aa8a4dcae3b8a1be204594fd1927e3edafd2bf0
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-09-22 15:14:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,23 @@
 # code your #position_taken? method here!
+
+def valid_move?(board, index)
+  if board[index] == " "
+    return false
+  elsif !position_taken(board, index)
+    return true
+  end
+end
+
+valid_move?(board, 0)
+
+board [" "," "," "," "," "," "," "," "," "]
+index = 0
+position_taken?(board, index)
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
